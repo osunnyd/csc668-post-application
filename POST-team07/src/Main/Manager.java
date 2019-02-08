e57aaddf8eeaf0b7d219fb759c6087938bd393ee
Janda95 (Mars) <j.rettinghouse@gmail.com>
2019-02-08 13:08:44 -0800

    read file and create catalog started

--- POST-team07/src/Main/Manager.java
+++ POST-team07/src/Main/Manager.java
@@ -1,4 +1,5 @@
 package Main;
+import java.util.ArrayList;
 
 //Jarek
 
@@ -24,8 +25,37 @@ public class Manager {
     //Method Close Store
 
     //Method tell store to set up Catalog, (or set up catalog and give it to store)
-    public void setUpCatalog(File file){
-        //Store.
+    public void setUpCatalog(File products){
+
+        //read file and distinguish different products in own object/struct
+        //1 product per line
+        //UPC (1-4)  Text description (10-29)  Price (35 to end of line)
+        //1234.....Item Description 1.......1.23
+
+        //ArrayList<Product>
+        Hashmap<String, Float> catalog = new HashMap<String, Float>();
+
+        FileInputStream in = null;
+
+        try {
+            in = new FileInputStream(products);
+
+            int c;
+            while ((c = in.read()) != -1) {
+                System.out.println("Stuff being read");
+            }
+
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+
+
+        //create hashmap with different products
+
+
+        //Store.setCatalog() = hashmap I just made
     }
 
 }
