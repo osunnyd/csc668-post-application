f7db2080f5b5ecf6140a0a7f3a787a45ad3dead6
Janda95 (Mars) <j.rettinghouse@gmail.com>
2019-02-08 11:36:46 -0800

    Main methods fields added but not fully implemented

--- POST-team07/src/Main/Manager.java
+++ POST-team07/src/Main/Manager.java
@@ -1,16 +1,31 @@
 package Main;
 
+//Jarek
+
 /*
-
-
-
-
+Manager is in charge of starting initialization.
+-Initialize Catalog
+-Set up Posts
+-Open the store
 */
 
 public class Manager {
     String name = "";
+    File products;
+    File transactions;
 
+    //Constructor
     Manager(String name){
-        this.name = name;
+        this.name = name
+    };
+
+    //Method Open store
+
+    //Method Close Store
+
+    //Method tell store to set up Catalog, (or set up catalog and give it to store)
+    public void setUpCatalog(File file){
+        //Store.
     }
+
 }
