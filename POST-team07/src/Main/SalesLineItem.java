a906740f8b96a40cc084147025a1c9073192366b
Tommy Tran <tmytrn@Tommys-MacBook-Pro-2.local>
2019-02-08 16:34:18 -0800

    implemented Item and SalesLineItem class

--- POST-team07/src/Main/SalesLineItem.java
+++ POST-team07/src/Main/SalesLineItem.java
@@ -1,4 +1,24 @@
 package Main;
 
 public class SalesLineItem {
+  private String upc;
+  private int quantity;
+  private double unitPrice;
+
+  public SalesLineItem(String upc, int quantity, double unitPrice){
+    this.upc = upc;
+    this.quantity = quantity;
+    this.unitPrice = unitPrice;
+
+  }
+
+  public String getUpc(){
+    return this.upc;
+  }
+  public int getQuantity(){
+    return this.quantity;
+  }
+  public double getUnitPrice(){
+    return this.unitPrice;
+  }
 }
