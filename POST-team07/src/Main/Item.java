a79d0dfe77b170d274fe751b91389c22e8164a72
Sunny Wong <swong6@mail.sfsu.edu>
2019-02-08 16:47:19 -0800

    completed Item class

--- POST-team07/src/Main/Item.java
+++ POST-team07/src/Main/Item.java
@@ -1,11 +1,51 @@
 package Main;
 
 /*
-
-
-
+Utility class item
 
 */
 
-public class Item {
+public class Item
+{
+  private String upc;
+  private String description;
+  private float price;
+
+  public Item(String upc, String description, float price)
+  {
+    this.upc = upc;
+    this.description = description;
+    this.price = price;
+  }
+
+  public String getUPC()
+  {
+    return this.upc;
+  }
+
+  public String getDescription()
+  {
+    return this.description;
+  }
+
+  public float getPrice()
+  {
+    return this.price;
+  }
+
+  public void setUPC(String setUPC)
+  {
+    upc = setUPC;
+  }
+
+  public void setDescription(String setDescription)
+  {
+    description = setDescription;
+  }
+
+  public void setPrice(float setPrice)
+  {
+    price = setPrice;
+  }
+
 }
--- POST-team07/src/Main/Store.java
+++ POST-team07/src/Main/Store.java
@@ -1,39 +1,45 @@
 package Main;
+
 import java.io.File;
 import java.util.HashMap;
 
-public class Store {
-    boolean isOpen = false;
+public class Store
+{
+  boolean isOpen = false;
 
-    // keeps track of UPC and quantity; assume quantity is infinite for now
-    HashMap<String, Integer> stock;
+  // keeps track of UPC and quantity; assume quantity is infinite for now
+  HashMap<String, Integer> stock;
 
-    //keeps track of UPC and what the Item is
-    HashMap<String, Item> catalog;
+  //keeps track of UPC and what the Item is
+  HashMap<String, Item> catalog;
 
-    POS pos;
-    SalesLog log;
+  POS pos;
+  SalesLog log;
 
-    public Store(File products) {
-        parseProducts(products);
-    }
+  public Store(File products)
+  {
+    parseProducts(products);
+  }
 
 
+  void writeToLog(POS pos, SalesLog log)
+  {
+  }
 
+  void parseProducts(File products)
+  {
 
-    void writeToLog(POS pos, SalesLog log) {}
+  }
 
-    void parseProducts(File products){}
+  boolean closeStore()
+  {
+    return isOpen = false;
+  }
 
-    boolean closeStore()
-    {
-        return isOpen = false;
-    }
-
-    boolean openStore()
-    {
-        return isOpen = true;
-    }
+  boolean openStore()
+  {
+    return isOpen = true;
+  }
 
 }
 
