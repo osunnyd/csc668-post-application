1645c436e9e503c8fb91fac604621e3bb146fc54
Sunny Wong <swong6@mail.sfsu.edu>
2019-02-08 21:40:32 -0800

    Finished parsing products

--- POST-team07/src/Main/Store.java
+++ POST-team07/src/Main/Store.java
@@ -1,30 +1,29 @@
 package Main;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Store {
+public class Store
+{
   boolean isOpen = false;
 
   // keeps track of UPC and quantity; assume quantity is infinite for now
-  HashMap<String, Integer> stock;
+  HashMap<String, Integer> stock = new HashMap<>();
 
   //keeps track of UPC and what the Item is
-  HashMap<String, Item> catalog;
+  HashMap<String, Item> catalog = new HashMap<>();
 
-  POS pos;
-  SalesLog log;
-  private String[] items;
+  private ArrayList<String> items = new ArrayList<>();
+
 
   public Store(File products) throws IOException {
     parseProducts(products);
   }
 
-  void writeToLog(POS pos, SalesLog log) {
-  }
 
   private void parseProducts(File products) throws IOException {
 
@@ -34,27 +33,30 @@ public class Store {
     FileReader readFile = new FileReader(products);
     BufferedReader lineBuffer = new BufferedReader(readFile);
 
-    while ( (item= lineBuffer.readLine()) != null )
+    while ( (item= lineBuffer.readLine()) != null ) // split each product to their own line
     {
-      items[itrOfItems] = item;
+      items.add(item);
       itrOfItems++;
     }
-      readFile.close();
-      lineBuffer.close();
-    }
+    readFile.close();
+    lineBuffer.close();
+    parseItems(items);
+  }
 
-    private void parseItems(String[] items){
+  private void parseItems(ArrayList items){
+    String[] product;
+
+    // parse each line by spaces
+    for (int itrOfItems = 0; itrOfItems < items.size(); itrOfItems++){
       Item item = new Item();
-
-      String[] product;
-
-      for (int itrOfItems = 0; itrOfItems < items.length; itrOfItems++){
-        product = items[itrOfItems].split(" ", 3);
-        item.setUPC(product[0]);
-        item.setDescription(product[1]);
-        item.setPrice(Float.parseFloat(product[2]));
-      }
+      product = items.get(itrOfItems).toString().split("  +"); // splits by 2 spaces or more
+      item.setUPC(product[0]);
+      item.setDescription(product[1]);
+      item.setPrice(Float.parseFloat(product[2]));
+      catalog.put(item.getUPC(), item);
+      stock.put(item.getUPC(), 900000); // assuming unlimited quantity for now
     }
+  }
 
   boolean closeStore() {
     return isOpen = false;
