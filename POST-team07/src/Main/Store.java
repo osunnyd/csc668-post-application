5ca67d45441bb73f1db51765fae4fc5df24b1881
omaralaniz <omar.alaniz021@gamil.com>
2019-02-08 16:52:26 -0800

    Temp finished parse of the products text file and started the split

--- POST-team07/src/Main/Store.java
+++ POST-team07/src/Main/Store.java
@@ -1,39 +1,65 @@
 package Main;
+
 import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class Store {
-    boolean isOpen = false;
+  boolean isOpen = false;
 
-    // keeps track of UPC and quantity; assume quantity is infinite for now
-    HashMap<String, Integer> stock;
+  // keeps track of UPC and quantity; assume quantity is infinite for now
+  HashMap<String, Integer> stock;
 
-    //keeps track of UPC and what the Item is
-    HashMap<String, Item> catalog;
+  //keeps track of UPC and what the Item is
+  HashMap<String, Item> catalog;
 
-    POS pos;
-    SalesLog log;
+  POS pos;
+  SalesLog log;
+  private String[] items;
 
-    public Store(File products) {
-        parseProducts(products);
-    }
+  public Store(File products) throws IOException {
+    parseProducts(products);
+  }
 
 
+  void writeToLog(POS pos, SalesLog log) {
+  }
 
+  private void parseProducts(File products) throws IOException {
 
-    void writeToLog(POS pos, SalesLog log) {}
+    String item;
+    int itrOfItems = 0;
 
-    void parseProducts(File products){}
+    FileReader readFile = new FileReader(products);
+    BufferedReader lineBuffer = new BufferedReader(readFile);
 
-    boolean closeStore()
+    while ( (item= lineBuffer.readLine()) != null )
     {
-        return isOpen = false;
+      items[itrOfItems] = item;
+      itrOfItems++;
+    }
+      readFile.close();
+      lineBuffer.close();
     }
 
-    boolean openStore()
-    {
-        return isOpen = true;
+    private void parseItems(String[] items){
+      Item item = new Item();
+      String[] product = new String[3];
+
+      for (int itrOfItems = 0; itrOfItems < items.length; itrOfItems++){
+        product = items[itrOfItems].split(" ", 3);
+      }
     }
+
+  boolean closeStore() {
+    return isOpen = false;
+  }
+
+  boolean openStore() {
+    return isOpen = true;
+  }
 
 }
 
