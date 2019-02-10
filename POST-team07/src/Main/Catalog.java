eb826b09b986e177ecb2dc32024914ca9ebd974b
Robert <rquinones93@yahoo.com>
2019-02-09 17:53:06 -0800

    Code spacing in Catalog

--- POST-team07/src/Main/Catalog.java
+++ POST-team07/src/Main/Catalog.java
@@ -7,50 +7,52 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Catalog
-{
+public class Catalog {
   //keeps track of UPC and what the Item is
   HashMap<UPC, Item> catalog = new HashMap<>();
 
   //each item is a String line to be parsed
   private ArrayList<String> items = new ArrayList<>();
 
-  public Catalog(File fileName) throws IOException
-  {
+  public Catalog(File fileName) throws IOException {
     parseProducts(fileName);
   }
-  public void parseProducts(File products) throws IOException
-  {
+
+  public void parseProducts( File products ) throws IOException {
 
     String item;
     int itrOfItems = 0;
 
-    FileReader readFile = new FileReader(products);
-    BufferedReader lineBuffer = new BufferedReader(readFile);
+    FileReader readFile = new FileReader( products );
+    BufferedReader lineBuffer = new BufferedReader( readFile );
 
-    while ( (item= lineBuffer.readLine()) != null ) // split each product to their own line
-    {
-      items.add(item);
+    while ( ( item = lineBuffer.readLine()) != null ) { // split each product to their own line
+      items.add( item );
       itrOfItems++;
     }
+
     readFile.close();
     lineBuffer.close();
-    parseItems(items);
+
+    parseItems( items );
   }
 
-  private void parseItems(ArrayList items){
+  private void parseItems( ArrayList items ) {
     String[] product;
 
     // parse each line by spaces
-    for (int itrOfItems = 0; itrOfItems < items.size(); itrOfItems++){
+    for ( int itrOfItems = 0; itrOfItems < items.size(); itrOfItems++ ) {
       Item item = new Item();
       UPC upc = new UPC();
-      product = items.get(itrOfItems).toString().split("  +"); // splits by 2 spaces or more
-      upc.setUPC(product[0]);
-      item.setUPC(product[0]);
-      item.setDescription(product[1]);
-      item.setPrice(Float.parseFloat(product[2]));
-      catalog.put(upc, item);
+
+      product = items.get( itrOfItems ).toString().split( "  +" ); // splits by 2 spaces or more
+
+      upc.setUPC( product[ 0 ] );
+      item.setUPC( product[ 0 ] );
+      item.setDescription( product[ 1 ] );
+      item.setPrice( Float.parseFloat( product[ 2 ] ) );
+
+      catalog.put( upc, item );
     }
   }
 
