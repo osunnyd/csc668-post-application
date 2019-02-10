04cc51216bc4a47f126470b854e91ae9849b7285
Sunny Wong <swong6@mail.sfsu.edu>
2019-02-09 18:12:37 -0800

    changed Object name from o to object for clarity

--- POST-team07/src/Main/Driver.java
+++ POST-team07/src/Main/Driver.java
@@ -13,48 +13,48 @@ import java.util.Scanner;
 
 public class Driver {
 
-    Manager manager = new Manager("JohnDoe");
-    //File products; -- Unsure if this should be removed?
-    //File transactions;
-    Store store;
-    // When True Prints to Console
-    static Boolean debugOn = false;
+  Manager manager = new Manager("JohnDoe");
+  //File products; -- Unsure if this should be removed?
+  //File transactions;
+  Store store;
+  // When True Prints to Console
+  static Boolean debugOn = false;
 
-    public static void main(String[] args) {
-        if ( args.length > 2 ) {
-            System.out.println("Invalid Amount of Command Line Arguments. Please try again.");
+  public static void main(String[] args) {
+    if ( args.length > 2 ) {
+      System.out.println("Invalid Amount of Command Line Arguments. Please try again.");
 
-        } else {
-            // Files are located in src/InputFiles/ -- DELETE this comment later
-            File products = new File( args[0] );
-            File transactions = new File( args[1] );
+    } else {
+      // Files are located in src/InputFiles/ -- DELETE this comment later
+      File products = new File( args[0] );
+      File transactions = new File( args[1] );
 
-            if ( debugOn ) {
-                printFileContents( products );
-                printFileContents( transactions );
-            }
+      if ( debugOn ) {
+        printFileContents( products );
+        printFileContents( transactions );
+      }
 
-            // TODO Manager Opens Store
-            // TODO Store Processes Transactions
-            // TODO Manager Closes Store
+      // TODO Manager Opens Store
+      // TODO Store Processes Transactions
+      // TODO Manager Closes Store
 
-            System.out.println( "\nEnd of Program" ); // DELETE LATER
-        }
+      System.out.println( "\nEnd of Program" ); // DELETE LATER
     }
+  }
 
-    // Debug Function - Delete later
-    public static void printFileContents( File file ) {
+  // Debug Function - Delete later
+  public static void printFileContents( File file ) {
 
-        try( Scanner tester = new Scanner( file ) ) {
+    try( Scanner tester = new Scanner( file ) ) {
 
-            while( tester.hasNextLine() ) {
-                System.out.println( tester.nextLine() );
-            }
+      while( tester.hasNextLine() ) {
+        System.out.println( tester.nextLine() );
+      }
 
-            System.out.println();
+      System.out.println();
 
-        } catch ( FileNotFoundException e ) {
-            System.out.println( e );
-        }
+    } catch ( FileNotFoundException e ) {
+      System.out.println( e );
     }
+  }
 }
