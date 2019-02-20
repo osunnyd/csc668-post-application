57acda2a141009e6355a33da4ad6836d05276511
unknown <jdvaldez@mail.sfsu.edu>
2019-02-20 11:42:52 -0800

    Added a createSaleJson that returns string for json for transaction, does not print properly needs fixing, pushing for opinions

--- src/PaymentAuthorizer/JsonBuilder.java
+++ src/PaymentAuthorizer/JsonBuilder.java
@@ -2,6 +2,7 @@ package PaymentAuthorizer;
 
 
 import Transaction.Transaction;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 //possibly delete this entire class if functions in other classes are better
@@ -13,18 +14,24 @@ public class JsonBuilder {
 
   }
 
-  public String checkAuthorizationJSON(Transaction Transaction) {
+  public String checkAuthorizationJSON(Transaction transaction) {
     JsonObject jsonObj = new JsonObject();
-    jsonObj.addProperty("amount", Transaction.getBillTotal());
+    jsonObj.addProperty("amount", transaction.getBillTotal());
     String json = jsonObj.toString();
     return json;
   }
 
-  public String creditAuthorizationJSON(Transaction Transaction) {
+  public String creditAuthorizationJSON(Transaction transaction) {
     JsonObject jsonObj = new JsonObject();
-    jsonObj.addProperty("amount", Transaction.getBillTotal());
-    jsonObj.addProperty("cardNumber", Transaction.getAmountTendered()); //change amount tendered to credit number
+    jsonObj.addProperty("amount", transaction.getBillTotal());
+    jsonObj.addProperty("cardNumber", transaction.getAmountTendered()); //change amount tendered to credit number
     String json = jsonObj.toString();
+    return json;
+  }
+
+  public String createSaleJson(Transaction transaction) {
+    Gson gson = new Gson();
+    String json = gson.toJson(transaction);
     return json;
   }
 }
